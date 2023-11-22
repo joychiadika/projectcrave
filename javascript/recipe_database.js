bf022bd5bcf04e8461aeb69647bb9202f55d1e68
Mitchellindev <mitchellin.tessy@gmail.com>
2023-11-22 13:25:40 +0100

    I added create account page, login page and recipe database page

---
+++ javascript/recipe_database.js
@@ -0,0 +1,8 @@
+
+
+// const ingredients = ["Butter", "Flour", "Parsley", "Pumpkin", "Fish", "Milk",''];
+// let list = document.querySelector(".kitchen_finds");
+
+// let buttons = ingredients.forEach((ingredient) => {
+// 	list.innerHTML += `<button type="button">${ingredient}</button>`;
+// });
